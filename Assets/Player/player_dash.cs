ee6a2479ae26a8ba333a114f6ffa3a2945e26ced
Nicoals <jugj@users.noreply.github.com>
2024-08-17 12:53:30 +0200

    Änderungen von Nicoals um 12:53:30

--- Assets/Player/player_dash.cs
+++ Assets/Player/player_dash.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class player_dash : MonoBehaviour
 {
     public float Geschwindichkeit;
+    private float dashkuldown;
+    public float dashtime;
     
     // Start is called before the first frame update
     void Start()
@@ -17,27 +19,33 @@ public class player_dash : MonoBehaviour
     {
         if(Input.GetKey("w")){
             transform.Translate(Vector2.up*Geschwindichkeit*Time.deltaTime,Space.World);
-            if(Input.GetKey("space")){
+            if(Input.GetKey("space") && dashkuldown > dashtime){
                 transform.Translate(Vector2.up*Geschwindichkeit*Time.deltaTime*10,Space.World);
+                dashkuldown = 0;
             }
         }
         if(Input.GetKey("s")){
             transform.Translate(Vector2.down*Geschwindichkeit*Time.deltaTime,Space.World);
-            if(Input.GetKey("space")){
+            if(Input.GetKey("space") &&dashkuldown > dashtime){
                 transform.Translate(Vector2.down*Geschwindichkeit*Time.deltaTime*10,Space.World);
+                dashkuldown = 0;
             }
         }
         if(Input.GetKey("d")){
             transform.Translate(Vector2.right*Geschwindichkeit*Time.deltaTime,Space.World);
-            if(Input.GetKey("space")){
+            if(Input.GetKey("space") && dashkuldown > dashtime){
                 transform.Translate(Vector2.right*Geschwindichkeit*Time.deltaTime*10,Space.World);
+                dashkuldown = 0;
             }
         }
         if(Input.GetKey("a")){
             transform.Translate(Vector2.left*Geschwindichkeit*Time.deltaTime,Space.World);
-            if(Input.GetKey("space")){
+            if(Input.GetKey("space") && dashkuldown > dashtime){
                 transform.Translate(Vector2.left*Geschwindichkeit*Time.deltaTime*10,Space.World);
+                dashkuldown = 0;
             }
         }
+
+        dashkuldown = dashkuldown + Time.deltaTime;
     }
 }
