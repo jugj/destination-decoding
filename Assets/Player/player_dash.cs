170b31f86d44ef88109bfa93cf344355b1a8a87e
Lars <jugj@users.noreply.github.com>
2024-08-17 20:42:13 +0200

    Änderungen von Lars um 20:42:13

--- Assets/Player/player_dash.cs
+++ Assets/Player/player_dash.cs
@@ -38,9 +38,9 @@ public class player_dash : MonoBehaviour
     }
 
     private void Dash(Vector2 vector, string animation){
-        rb.AddForce(vector * Geschwindichkeit, ForceMode2D.Impulse);
+        rb.AddForce(vector * Geschwindichkeit * Time.deltaTime, ForceMode2D.Impulse);
             if(Input.GetKey("space") && dashkuldown > dashtime){
-                rb.AddForce(vector * 20 * Geschwindichkeit, ForceMode2D.Impulse);
+                rb.AddForce(vector * 20 * Geschwindichkeit * Time.deltaTime, ForceMode2D.Impulse);
                 dashkuldown = 0;
 
                 anim.SetTrigger(animation);
