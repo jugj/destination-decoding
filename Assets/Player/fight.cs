c1d70ac2939d6664d88e7d8328c9e1afada5fc75
Jano <jugj@users.noreply.github.com>
2024-08-18 14:54:52 +0200

    Änderungen von Jano um 14:54:52

--- Assets/Player/fight.cs
+++ Assets/Player/fight.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManager;
+using UnityEngine.SceneManagements;
 
 public class fight : MonoBehaviour
 {
